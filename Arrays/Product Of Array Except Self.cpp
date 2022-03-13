76523f2ace1d2385848b7be03e94d6fd56c77e08
satwik <satwikraj2@gmail.com>
2022-03-13 14:08:38 +0530

    13/3/22

--- Arrays/Product Of Array Except Self.cpp
+++ Arrays/Product Of Array Except Self.cpp
@@ -2,34 +2,66 @@
 using namespace std;
 
 int mod = 100000007;
-// Approach - 1
-int *getProductArrayExceptSelf(int *arr, int n)
-{
-    //Write your code here
-    int ans[n];
-    int m = 0;
+// Approach - 1 (TC = O(N^2) + O(N))
 
-    //long long prod = 1;
 
-    for(int i = 0 ; i < n ; i++){
-    	long long prod = 1;
-    	for(int j = 0 ; j < n ; j++){
-    		if(j != i){
-    			prod = (prod * arr[j]) % mod;
-    		}
-    	}
-    	ans[m++] = prod;
-    }
-    for(int i = 0 ; i < n ; i++){
-    	arr[i] = ans[i];
-    }
+// int *getProductArrayExceptSelf(int *arr, int n)
+// {
+//     //Write your code here
+//     int *ans = new int[n];
+//     int m = 0;
 
-    return arr;
-}
+//     //long long prod = 1;
+
+//     for(int i = 0 ; i < n ; i++){
+//     	long long prod = 1;
+//     	for(int j = 0 ; j < n ; j++){
+//     		if(j != i){
+//     			prod = prod % mod;
+//     			prod = (prod * (arr[j] % mod)) % mod;
+//     		}
+//     	}
+//     	ans[i] = prod;
+//     }
+
+//     return ans;
+// }
 
 
 //Approach - 2
+//using 2 arrays (one for prefix and one for suffix);
 
+int multiply(int a, int b){
+
+	long long ret = a % mod;
+	ret = (ret * (b % mod)) % mod;
+
+	return ret;
+}
+
+//TC = O(N), SC = O(N) for output array.
+int *getarray(int *arr, int n){
+
+	long long prod = 1;
+
+	int *output = new int[n];
+
+//O(N)
+	for(int i = 0 ; i < n ; i++){
+		output[i] = prod;
+		prod = multiply(prod, arr[i]);
+	}
+
+//O(N)
+	prod = 1;
+
+	for(int i = n-1 ; i >=0 ; i--){
+		output[i] = (output[i] * prod);
+		prod = multiply(prod, arr[i]);
+	}
+
+	return output;
+}
 
 
 
@@ -53,7 +85,7 @@ int main(){
 			cin >> arr[i];
 		}
 
-		int *x = getProductArrayExceptSelf(arr, n);
+		int *x = getarray(arr, n);
 
 		for (int i = 0; i < n; ++i)
 		{
